38012076269be9d8b381e44d3ac77c5f4b439a87
Yohanan <yohananjr13@gmail.com>
2015-06-20 17:59:23 +0700

    add db, models

---
+++ api/models/Product.js
@@ -0,0 +1,23 @@
+/**
+* Product.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	name: {
+  		type: 'STRING',
+  		required: true,
+  		minLength: 2
+  	},
+  	image: 'STRING',
+  	catId: {
+  		model: 'category',
+  		columnName: 'cat_id'
+  	}
+  }
+};
+
